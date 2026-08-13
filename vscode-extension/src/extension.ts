973170a9a154fb2e8d0c3b7a11128d0512455714
Tuncay Çelikkanat <tuncayclkknt@gmail.com>
2026-08-13 17:03:08 +0300

    feat(vscode): configure warden.scan command in package.json and extension.ts

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('warden.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('warden.scan', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Warden!');
+		vscode.window.showInformationMessage('Warden taraması başlatılıyor...');
 	});
 
 	context.subscriptions.push(disposable);
